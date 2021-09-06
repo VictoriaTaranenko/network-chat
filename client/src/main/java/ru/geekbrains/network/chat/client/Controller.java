487a6943e660fa04b08b9999876eb2738e48888d
VictoriaTaranenko <t.victoria-dev@mail.ru>
2021-09-06 09:17:32 +0300

    add lesson2

--- client/src/main/java/ru/geekbrains/network/chat/client/Controller.java
+++ client/src/main/java/ru/geekbrains/network/chat/client/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -15,46 +16,111 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller  {
     @FXML
-    TextField msgField;
+    TextField msgField,usernameField;
 
     @FXML
     TextArea msgArea;
 
+    @FXML
+    HBox loginPanel, msgPanel;
+
+
+
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private String username;
 
-    public void initialize(URL location, ResourceBundle resources) {
-       try {
-           socket = new Socket("localhost",8189);
-           in = new DataInputStream(socket.getInputStream());
-           out = new DataOutputStream(socket.getOutputStream());
-           Thread t = new Thread(()-> {
-               try {
-                   while (true) {
-                       String msg = in.readUTF();
-                       msgArea.appendText(msg + '\n');
-                   }
-               } catch (IOException e) {
-                   e.printStackTrace();
-               }
-           });
-           t.start();
-       } catch(IOException e) {
-           throw new RuntimeException("Unable to connect to server [localhost:8189 ]");
-       }
+    public void setUsername(String username) {
+        this.username = username;
+        if(username != null) {
+            loginPanel.setVisible(false);
+            loginPanel.setManaged(false);
+            msgPanel.setVisible(true);
+            msgPanel.setManaged(true);
+        } else {
+            loginPanel.setVisible(true);
+            loginPanel.setManaged(true);
+            msgPanel.setVisible(false);
+            msgPanel.setManaged(false);
+        }
     }
 
-    public void sendMsg() throws IOException {
-        try {
+    public void login() {
+    if(socket == null || socket.isClosed()) {
+        connect();
+    }
+    if(usernameField.getText().isEmpty()) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Имя пользователя не может быть пустым",ButtonType.OK);
+        alert.showAndWait();
+        return;
 
+    }
+        try {
+            out.writeUTF("/login" + usernameField.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
+        try {
+            socket = new Socket("localhost",8189);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            Thread t = new Thread(()-> {
+                try {
+                    // цикл авторизации
+                    while (true) {
+                        String msg = in.readUTF();
+                        if(msg.startsWith("/login_ok")) {
+                            setUsername (msg.split("\\s")[1]);
+                            break;
+                            }
+                        if(msg.startsWith("/login_failed")) {
+                            String cause = msg.split("\\s",2)[1];
+                            msgArea.appendText(cause + '\n');
+                        }
+                        }
+                    // цикл общения
+                    while (true) {
+                        String msg = in.readUTF();
+                        msgArea.appendText(msg + '\n');
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    disconnect();
+                }
+            });
+            t.start();
+        } catch(IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно подключиться к серверу ",ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+    public void sendMsg()  {
+        try {
             out.writeUTF(msgField.getText());
             msgField.clear();
+            msgField.requestFocus();
         } catch(IOException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR," Невозможно отправить сообщение", ButtonType.OK);
             alert.showAndWait();
+        }
+    }
+
+    private void disconnect() {
+        setUsername(null);
+        try {
+            if(socket != null) {
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
--- server/src/main/java/ru/geekbrains/network/chat/server/ServerApp.java
+++ server/src/main/java/ru/geekbrains/network/chat/server/ServerApp.java
@@ -1,43 +1,8 @@
 package ru.geekbrains.network.chat.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ServerApp {
 
-    /* 1.пусть сервер подсчитывает количество сообщений от клиента
-       2.Если клиент отправил команду '/stat' , то сервер должен выслать клиенту не эхо ,
-         а сообщение вида " Количество сообщений - n"
-    */
     public static void main(String[] args)  {
-        try (ServerSocket serverSocket = new ServerSocket(8189)) {
-            System.out.println(" Сервер запущен на порту 8189. Ожидаем подключение клиента... ");
-            Socket socket = serverSocket.accept();
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-            System.out.println(" Клиент подключился ");
-            int msgCounter = 0;
-
-            while (true) {
-                String msg = in.readUTF();
-                System.out.print(msg);
-                if(msg.startsWith("/")) {
-                    if(msg.equals("/stat")) {
-                        out.writeUTF("Messages count: " + msgCounter);
-                        continue;
-                    }
-                }
-                out.writeUTF("ECHO:" + msg);
-                msgCounter++;
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+       new Server(8189);
     }
 }
